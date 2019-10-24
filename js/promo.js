ed999afd964fb7fbd50aa2754f2fe7a65ed54bd3
Alexander Tikhonov <at1385@yandex.ru>
2019-10-25 00:04:19 +0500

    Исправляет синтаксис кода promo.js

--- js/promo.js
+++ js/promo.js
@@ -12,19 +12,19 @@ promoButton1.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (!promoButton1.classList.contains("promo-slider-button-current")) {
     if (promoButton2.classList.contains("promo-slider-button-current")) {
-     promoButton2.classList.remove("promo-slider-button-current")
+     promoButton2.classList.remove("promo-slider-button-current");
    } else {
-     promoButton3.classList.remove("promo-slider-button-current")
+     promoButton3.classList.remove("promo-slider-button-current");
    }
-    promoButton1.classList.add("promo-slider-button-current")
+    promoButton1.classList.add("promo-slider-button-current");
   }
   if (!promoSlide1.classList.contains("promo-slides-item-show")) {
     if (promoSlide2.classList.contains("promo-slides-item-show")) {
-     promoSlide2.classList.remove("promo-slides-item-show")
+     promoSlide2.classList.remove("promo-slides-item-show");
    } else {
-     promoSlide3.classList.remove("promo-slides-item-show")
+     promoSlide3.classList.remove("promo-slides-item-show");
    }
-    promoSlide1.classList.add("promo-slides-item-show")
+    promoSlide1.classList.add("promo-slides-item-show");
   }
 });
 
@@ -32,19 +32,19 @@ promoButton2.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (!promoButton2.classList.contains("promo-slider-button-current")) {
     if (promoButton1.classList.contains("promo-slider-button-current")) {
-     promoButton1.classList.remove("promo-slider-button-current")
+     promoButton1.classList.remove("promo-slider-button-current");
    } else {
-     promoButton3.classList.remove("promo-slider-button-current")
+     promoButton3.classList.remove("promo-slider-button-current");
    }
-    promoButton2.classList.add("promo-slider-button-current")
+    promoButton2.classList.add("promo-slider-button-current");
   }
   if (!promoSlide2.classList.contains("promo-slides-item-show")) {
     if (promoSlide1.classList.contains("promo-slides-item-show")) {
-     promoSlide1.classList.remove("promo-slides-item-show")
+     promoSlide1.classList.remove("promo-slides-item-show");
    } else {
-     promoSlide3.classList.remove("promo-slides-item-show")
+     promoSlide3.classList.remove("promo-slides-item-show");
    }
-    promoSlide2.classList.add("promo-slides-item-show")
+    promoSlide2.classList.add("promo-slides-item-show");
   }
 });
 
@@ -52,18 +52,18 @@ promoButton3.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (!promoButton3.classList.contains("promo-slider-button-current")) {
     if (promoButton1.classList.contains("promo-slider-button-current")) {
-     promoButton1.classList.remove("promo-slider-button-current")
+     promoButton1.classList.remove("promo-slider-button-current");
    } else {
-     promoButton2.classList.remove("promo-slider-button-current")
+     promoButton2.classList.remove("promo-slider-button-current");
    }
-    promoButton3.classList.add("promo-slider-button-current")
+    promoButton3.classList.add("promo-slider-button-current");
   }
   if (!promoSlide3.classList.contains("promo-slides-item-show")) {
     if (promoSlide1.classList.contains("promo-slides-item-show")) {
-     promoSlide1.classList.remove("promo-slides-item-show")
+     promoSlide1.classList.remove("promo-slides-item-show");
    } else {
-     promoSlide2.classList.remove("promo-slides-item-show")
+     promoSlide2.classList.remove("promo-slides-item-show");
    }
-    promoSlide3.classList.add("promo-slides-item-show")
+    promoSlide3.classList.add("promo-slides-item-show");
   }
 });
